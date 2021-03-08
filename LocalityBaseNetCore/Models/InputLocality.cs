221aa7fb921366ad300468f89bf31a5e466e1ebd
Yan Furman <furmanyan@gmail.com>
2021-03-08 19:48:38 +0300

    Added input form

---
+++ LocalityBaseNetCore/Models/InputLocality.cs
@@ -0,0 +1,19 @@
+namespace LocalityBaseNetCore.Models
+{
+    public class InputLocality
+    {
+        public int id { get; set; }
+        
+        public string Type { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Submission { get; set; }
+        
+        public string PeopleCount { get; set; }
+        
+        public string Budget { get; set; }
+        
+        public string Headman { get; set; }
+    }
+}
