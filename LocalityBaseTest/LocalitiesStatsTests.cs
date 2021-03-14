ff35813ec855bdacf6890c90e31f5e2beb8b1aaf
Yan Furman <furmanyan@gmail.com>
2021-03-14 22:46:34 +0300

    Added tests for function GetOverallBudget

--- LocalityBaseTest/LocalitiesStatsTests.cs
+++ LocalityBaseTest/LocalitiesStatsTests.cs
@@ -71,5 +71,68 @@ namespace LocalityBaseTest
             //Assert
             Assert.Equal(ExpectededResult, OverallPeople);
         }
+        
+        
+        [Fact]
+        public void GetOverallBudgetNullList()
+        {
+            //Arrange
+            List<Locality> locs = null;
+            decimal ExpectededResult = 0;
+            //Act
+            decimal OverallPeople = GetOverallBudget(locs);
+            //Assert
+            Assert.Equal(ExpectededResult, OverallPeople);
+        }
+        
+        
+        [Fact]
+        public void GetOverallBudgetEmptyList()
+        {
+            //Arrange
+            List<Locality> locs = new List<Locality>();
+            decimal ExpectededResult = 0;
+            //Act
+            decimal OverallPeople = GetOverallBudget(locs);
+            //Assert
+            Assert.Equal(ExpectededResult, OverallPeople);
+        }
+        
+        
+        [Fact]
+        public void GetOverallBudgetOneLocInList()
+        {
+            //Arrange
+            List<Locality> locs = new List<Locality>();
+            Locality loc = new Locality();
+            loc.Budget = 100;
+            locs.Add(loc);
+            decimal ExpectededResult = 100;
+            //Act
+            decimal OverallPeople = GetOverallBudget(locs);
+            //Assert
+            Assert.Equal(ExpectededResult, OverallPeople);
+        }
+        
+        
+        [Fact]
+        public void GetOverallBudgetMultipleLocsInList()
+        {
+            //Arrange
+            List<Locality> locs = new List<Locality>();
+            decimal ExpectededResult = 0;
+            for (int i = 10; i <= 100; i+=10)
+            {
+                Locality loc = new Locality();
+                decimal rand = new decimal( new Random().NextDouble() * i );
+                ExpectededResult += rand;
+                loc.Budget = rand;
+                locs.Add(loc);
+            }
+            //Act
+            decimal OverallPeople = GetOverallBudget(locs);
+            //Assert
+            Assert.Equal(ExpectededResult, OverallPeople);
+        }
     }
 }
