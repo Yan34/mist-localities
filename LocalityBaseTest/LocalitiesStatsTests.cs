bd543a2a8ce7435a14782ba28ee72a46e1515205
Yan Furman <furmanyan@gmail.com>
2021-03-14 22:59:30 +0300

    Added tests for function GetAverageBudget

--- LocalityBaseTest/LocalitiesStatsTests.cs
+++ LocalityBaseTest/LocalitiesStatsTests.cs
@@ -205,5 +205,72 @@ namespace LocalityBaseTest
             //Assert
             Assert.Equal(ExpectededResult, OverallPeople);
         }
+        
+        
+        //GetAverageBudget tests
+        
+        [Fact]
+        public void GetAverageBudgetNullList()
+        {
+            //Arrange
+            List<Locality> locs = null;
+            decimal ExpectededResult = 0;
+            //Act
+            decimal AveragePeople = GetAverageBudget(locs);
+            //Assert
+            Assert.Equal(ExpectededResult, AveragePeople);
+        }
+        
+        
+        [Fact]
+        public void GetAverageBudgetEmptyList()
+        {
+            //Arrange
+            List<Locality> locs = new List<Locality>();
+            decimal ExpectededResult = 0;
+            //Act
+            decimal AveragePeople = GetAverageBudget(locs);
+            //Assert
+            Assert.Equal(ExpectededResult, AveragePeople);
+        }
+        
+        
+        [Fact]
+        public void GetAverageBudgetOneLocInList()
+        {
+            //Arrange
+            List<Locality> locs = new List<Locality>();
+            Locality loc = new Locality();
+            loc.Budget = 100;
+            locs.Add(loc);
+            decimal ExpectededResult = 100;
+            //Act
+            decimal AverageBudget = GetAverageBudget(locs);
+            //Assert
+            Assert.Equal(ExpectededResult, AverageBudget);
+        }
+        
+        
+        [Fact]
+        public void GetAverageBudgetMultipleLocsInList()
+        {
+            //Arrange
+            List<Locality> locs = new List<Locality>();
+            decimal sum = 0;
+            int count = 10;
+            for (int i = 10; i <= 100; i+=10)
+            {
+                Locality loc = new Locality();
+                decimal rand = new decimal( new Random().NextDouble() * i );
+                sum += rand;
+                loc.Budget = rand;
+                locs.Add(loc);
+            }
+            decimal ExpectededResult = decimal.Round( decimal.Divide(sum, count), 3 );
+            //Act
+            decimal AverageBudget = GetAverageBudget(locs);
+            //Assert
+            Assert.Equal(ExpectededResult, AverageBudget);
+        }
     }
 }
