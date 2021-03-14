80360263fbbbd8696bd169cc4ba9bb2d19265d89
Yan Furman <furmanyan@gmail.com>
2021-03-14 22:55:42 +0300

    Added tests for function GetAveragePeople

--- LocalityBaseTest/LocalitiesStatsTests.cs
+++ LocalityBaseTest/LocalitiesStatsTests.cs
@@ -10,6 +10,8 @@ namespace LocalityBaseTest
 {
     public class LocalitiesStatsTests
     {
+        //GetOverallPeople tests
+        
         [Fact]
         public void GetOverallPeopleNullList()
         {
@@ -72,6 +74,75 @@ namespace LocalityBaseTest
             Assert.Equal(ExpectededResult, OverallPeople);
         }
         
+        
+        //GetAveragePeople tests
+        
+        [Fact]
+        public void GetAveragePeopleNullList()
+        {
+            //Arrange
+            List<Locality> locs = null;
+            decimal ExpectededResult = 0;
+            //Act
+            decimal AveragePeople = GetAveragePeople(locs);
+            //Assert
+            Assert.Equal(ExpectededResult, AveragePeople);
+        }
+        
+        
+        [Fact]
+        public void GetAveragePeopleEmptyList()
+        {
+            //Arrange
+            List<Locality> locs = new List<Locality>();
+            decimal ExpectededResult = 0;
+            //Act
+            decimal AveragePeople = GetAveragePeople(locs);
+            //Assert
+            Assert.Equal(ExpectededResult, AveragePeople);
+        }
+        
+        
+        [Fact]
+        public void GetAveragePeopleOneLocInList()
+        {
+            //Arrange
+            List<Locality> locs = new List<Locality>();
+            Locality loc = new Locality();
+            loc.PeopleCount = 100;
+            locs.Add(loc);
+            decimal ExpectededResult = 100;
+            //Act
+            decimal AveragePeople = GetAveragePeople(locs);
+            //Assert
+            Assert.Equal(ExpectededResult, AveragePeople);
+        }
+        
+        
+        [Fact]
+        public void GetAveragePeopleMultipleLocsInList()
+        {
+            //Arrange
+            List<Locality> locs = new List<Locality>();
+            decimal sum = 0;
+            int count = 10;
+            for (int i = 10; i <= 100; i+=10)
+            {
+                Locality loc = new Locality();
+                decimal rand = new decimal( new Random().NextDouble() * i );
+                sum += rand;
+                loc.PeopleCount = rand;
+                locs.Add(loc);
+            }
+            decimal ExpectededResult = decimal.Round( decimal.Divide(sum, count), 3 );
+            //Act
+            decimal AveragePeople = GetAveragePeople(locs);
+            //Assert
+            Assert.Equal(ExpectededResult, AveragePeople);
+        }
+        
+        
+        //GetOverallBudget tests
         
         [Fact]
         public void GetOverallBudgetNullList()
